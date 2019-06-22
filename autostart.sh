525b44bd80eb5d622ba570373328202a5c142684
manfrommar <46165335+manfrommar@users.noreply.github.com>
2019-06-22 23:17:04 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-### In opensim.sh (make sure this file is chmod +x):
-
-
+### make sure this file is chmod +x 
 cd /opt/opensim/bin/
 rm *.log
-#exec screen -S money -D -m mono ./MoneyServer.exe
-screen -DmS money mono ./MoneyServer.exe
-
-
+screen -DmS opensim mono ./OpenSim.exe 
